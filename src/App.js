37b08a7106d71e07926199acd82cce904a6fef3a
wootaepark <pakdal99@gmail.com>
2024-03-20 00:27:44 +0900

    ch5_commit

--- src/App.js
+++ src/App.js
@@ -1,43 +1,56 @@
 import logo from './logo.svg';
 import './App.css';
 
-// 사용자정의 태그 == 컴포넌트
-function Header(){
+// 사용자정의 태그 == 컴포넌트 -> 이러한 컴포넌트를 여러 사람이 공유하여 사용 가능하고, 간략하다는 장점이 있다.
+function Header(props){
+  console.log('props', props.title);
   return(
     <header>
-      <h1><a href="/">React</a></h1>
+      <h1><a href="/">{props.title}</a></h1>
     </header>
   )
 }
 
-function Nav(){
+function Nav(props){
+  const lis = [];
+
+  for(let i = 0;i<props.topics.length; i++){
+    let t = props.topics[i];
+    lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a> {t.body}</li>
+    );
+  }
   return(
     <nav>
     <ol>
-      <li><a href="/read/1">html</a></li>
-      <li><a href="/read/2">css</a></li>
-      <li><a href="/read/3">js</a></li>
+      {lis}
     </ol>
   </nav>
   )
 }
 
-function Article(){
+function Article(props){
+  console.log('props', props.title, props.body);
   return(
     <article>
-    <h2>Welcome</h2>
-    Hello, WEB
+    <h2>{props.title}</h2>
+    {props.body}
   </article>
   )
 }
 
 function App() {
+  const topics = [
+    {id:1, title:'html', body:'html is ...'},
+    {id:2, title:'css', body: 'css is ...'},
+    {id:3, title:'javascript', body: 'javascript is ...'}
+  ];
   return ( // <Header/>로 표현해도 됨 
     <div>
 
-    <Header></Header>
-    <Nav></Nav>
-    <Article></Article>
+    <Header title="WEB"></Header>
+    <Nav topics={topics}></Nav>
+    <Article title="Welcome" body="Hello, WEB"></Article>
+    
 
     </div>
 
