2b4f6134186deb43f918b6ead9ff48dd693de2b3
wootaepark <pakdal99@gmail.com>
2024-03-19 23:15:21 +0900

    ch4_commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
 
+// 사용자정의 태그 == 컴포넌트
+function Header(){
+  return(
+    <header>
+      <h1><a href="/">React</a></h1>
+    </header>
+  )
+}
+
+function Nav(){
+  return(
+    <nav>
+    <ol>
+      <li><a href="/read/1">html</a></li>
+      <li><a href="/read/2">css</a></li>
+      <li><a href="/read/3">js</a></li>
+    </ol>
+  </nav>
+  )
+}
+
+function Article(){
+  return(
+    <article>
+    <h2>Welcome</h2>
+    Hello, WEB
+  </article>
+  )
+}
+
 function App() {
-  return (
-    <div className="App">
-      Hello React!
+  return ( // <Header/>로 표현해도 됨 
+    <div>
+
+    <Header></Header>
+    <Nav></Nav>
+    <Article></Article>
+
     </div>
+
+    
   );
 }
 
