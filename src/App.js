779caca5aa5f4e497da21efb83e6ec1076c02297
wootaepark <pakdal99@gmail.com>
2024-03-22 21:35:13 +0900

    ch7-status-commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
 
 // 사용자정의 태그 == 컴포넌트 -> 이러한 컴포넌트를 여러 사람이 공유하여 사용 가능하고, 간략하다는 장점이 있다.
 function Header(props){
@@ -24,7 +25,7 @@ function Nav(props){
     lis.push(<li key={t.id}>
       <a id={t.id} href={'/read/'+t.id} onClick={event=>{
       event.preventDefault();
-      props.onChangeMode(event.target.id);// t.id 를 매개변수로 넣어도 동일 동작
+      props.onChangeMode(Number(event.target.id));// t.id 를 매개변수로 넣어도 동일 동작
     }}>{t.title}</a></li>
     );
   }
@@ -48,21 +49,50 @@ function Article(props){
 }
 
 function App() {
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
+
+  console.log('_mode', mode);
   const topics = [
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body: 'css is ...'},
     {id:3, title:'javascript', body: 'javascript is ...'}
   ];
+
+  let content = null;
+
+  console.log('Mode1 : ', mode, ' Mode2 : ', id);
+  if(mode === 'WELCOME'){
+    content = <Article title="Welcome" body="Hello, Web"></Article>
+  }
+  else if(mode === 'READ'){ 
+    let title, body = null;
+    for (let i = 0; i<topics.length ;i++){
+      console.log(topics[i].id, id);
+      if(topics[i].id === id){
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>
+    
+  }
+
+ 
+
   return ( // <Header/>로 표현해도 됨 
     <div>
 
     <Header title="WEB" onChangeMode={()=>{
-      alert('Header');
+      setMode('WELCOME');
     }}></Header>
-    <Nav topics={topics} onChangeMode={(id)=>{
-      alert(id);  
+
+    <Nav topics={topics} onChangeMode={(_id)=>{
+      setMode('READ');
+      setId(_id);
+      // status가 두개인 것이다.
     }}></Nav>
-    <Article title="Welcome" body="Hello, WEB"></Article>
+    {content}
     
 
     </div>
